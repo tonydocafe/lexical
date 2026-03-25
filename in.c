a4c84367930b7e06c60fef161a0a9c7b63e3c4b4
tonydocafe <mneoacme@gmail.com>
2026-03-25 20:40:30 -0300

    up code

---
+++ in.c
@@ -0,0 +1,16 @@
+int x = 5;
+int z = 0;
+
+int num(x){
+
+    printf("O numero é: %d\n", x);
+while(x>0){
+    x--;     
+
+
+}
+int main() {
+    z = x + 10;
+    return 0;
+}//eae tranquilo 
+sdsaweew
